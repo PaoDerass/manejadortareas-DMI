de14e5c1621dff2ccc53e035fa45aef9978d3376
PaoDerass <paola_deras@unitec.edu>
2025-02-16 21:12:20 -0600

    Agregar la pantalla Buscador a la navegación

--- Componentes/Navegacion.tsx
+++ Componentes/Navegacion.tsx
@@ -1,22 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { NavigationContainer } from '@react-navigation/native'
-import Home from '../Paginas/Home'
-import AgregarTarea from '../Paginas/AgregarTarea'
-import Buscador from '../Paginas/Buscador'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from '../Paginas/Home';
+import AgregarTarea from '../Paginas/AgregarTarea';
+import Buscador from '../Paginas/Buscador';
 
 export default function Navegacion() {
-
-  const Tab = createBottomTabNavigator()
+  const Tab = createBottomTabNavigator();
 
   return (
     <NavigationContainer>
-        <Tab.Navigator>
-            <Tab.Screen name='Home' component={Home}></Tab.Screen>
-            <Tab.Screen name='Agregar Tarea' component={AgregarTarea}></Tab.Screen>
-            <Tab.Screen name='Buscador' component={Buscador}></Tab.Screen>
-        </Tab.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Agregar Tarea" component={AgregarTarea} />
+        <Tab.Screen name="Buscador" component={Buscador} />
+      </Tab.Navigator>
     </NavigationContainer>
-  )
+  );
 }
